32ac979d4ba089fd50d2c227a661803f395bd0f9
Zhyryll <zhyryllposadas@gmail.com>
2023-07-20 11:52:08 +0800

    Using dirname and filename

--- global.js
+++ global.js
@@ -1,15 +1,22 @@
 // console.log(global);
 
-
+//SETTIMEOUT - mag run only once depending kung ilang segundo mo siya ise-set
 // setTimeout(()=>{
 //     console.log(`ITS BEEN 5 SECONDS`);
 // }, 5000)
 
-const int = setInterval(()=>{
-    console.log(`Its gonna shut`);
-}, 1000);
 
-setTimeout(()=>{
-    console.log('The interval will stop HERE!');
-    clearInterval(int);
-}, 5100);
+//SETINTERVAL - mag ra-run siya depending sa time interval (segundo/millisecond) na naka set
+// const int = setInterval(()=>{
+//     console.log(`Its gonna shut`);
+// }, 1000);
+
+// setTimeout(()=>{
+//     console.log('The interval will stop HERE!');
+//     clearInterval(int);
+// }, 5100);
+
+
+console.log(__dirname); //After running the node global, ipapakita nito yung path or directory kung saan nakalagay yung file
+console.log(__filename); //After running the node globa, ipapakita nito yung currect directory pati narin yung file name
+
