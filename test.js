5fea59ce07f60e32b177c79ab3994157c8696a21
Zhyryll <zhyryllposadas@gmail.com>
2023-07-20 11:24:04 +0800

    Using arrow function and running it using node cli

---
+++ test.js
@@ -0,0 +1,21 @@
+// const name = "Ella Mae Lorenzo Calugay";
+
+// console.log(name);
+
+
+// const greet = (pangalan) => {
+//     console.log(`Hello, ${pangalan}`);
+// }
+
+// greet("Zhyryll Posadas");
+// greet("Ella Calugay");
+
+const sum = (num) => {
+    if(num%2==0){
+        console.log(`${num} is EVEN`);
+    }else{
+        console.log(`${num} is ODD`);
+    }
+}
+
+sum(5);
