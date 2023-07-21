6e35997a98ae94bac49200868aac993e66b75003
Zhyryll <zhyryllposadas@gmail.com>
2023-07-21 20:05:43 +0800

    Added OS and logline os.platform() and os.homedir()

--- modules.js
+++ modules.js
@@ -1,7 +1,10 @@
-//IMPORT_1 - naka-import and naka store sa isang variable.
+//IMPORT_1 - naka import but hindi siya magagamit for this file kasi hindi naka store sa variable
+require('./persons');
+
+//IMPORT_2 - naka-import and naka store sa isang variable.
 const imprt = require('./persons');
 
-//IMPORT_2 - pwede lang mag import ng ganito kung yung naka export sa kabilang file is naka object. Kung ano lang yung nakalagay na property, yun lang yung maa-access
+//IMPORT_3 - pwede lang mag import ng ganito kung yung naka export sa kabilang file is naka object. Kung ano lang yung nakalagay na property, yun lang yung maa-access
 const {fnames, ages, trigger} = require('./persons');
 
 
@@ -14,5 +17,7 @@ console.log(imprt);
 console.log(fnames, ages, trigger());
 
 
-
+const os = require('os');
+//Ipapakita niya lang yung type ng OS and Home directory
+console.log(os.platform(), os.homedir());
 // console.log(persons);
