fc59b122547e7202ac006755d447940908fcfa48
Zhyryll <zhyryllposadas@gmail.com>
2023-07-22 11:01:24 +0800

    Can now delete file using unlink() method

--- files.js
+++ files.js
@@ -30,36 +30,56 @@ const fs = require('fs');
 
 
 //CREATE/DELETE DIRECTORIES
-// Example_1: Add directory
-fs.mkdir('./assets', (err) => {
-    if(err){
-        console.log(err);
-    }
-    console.log('Folder Created');
-});
-//Example_2: Remove directory
-fs.rmdir('./assets', (err) => {
-    if(err){
-        console.log(err);
-    }
-    console.log('Folder Deleted');
-});
-//Example_3: Create directory if it dont exist but delete directory if it exist using if-else conditional statement
-if(!fs.existsSync('./assets')){
-    fs.mkdir('./assets', (err) => {
-       if(err){
-        console.log(err);
-       }
-       console.log('Folder Created');
-    });
-}else{
-    fs.rmdir('./assets', (err) => {
-        if(err){
-            console.log(err);
-        }
-        console.log('Folder Deleted');
-    });
-}
+// // Example_1: Add directory
+// fs.mkdir('./assets', (err) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log('Folder Created');
+// });
+// //Example_2: Remove directory
+// fs.rmdir('./assets', (err) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log('Folder Deleted');
+// });
+// //Example_3: Create directory if it dont exist but delete directory if it exist using if-else conditional statement
+// if(!fs.existsSync('./assets')){
+//     fs.mkdir('./assets', (err) => {
+//        if(err){
+//         console.log(err);
+//        }
+//        console.log('Folder Created');
+//     });
+// }else{
+//     fs.rmdir('./assets', (err) => {
+//         if(err){
+//             console.log(err);
+//         }
+//         console.log('Folder Deleted');
+//     });
+// }
 
 
 //DELETE FILES
+//Example_1: Delete file directly
+fs.unlink('./documents/deleteme.txt', (err) => {
+    if(err){
+        console.log(err)
+    }
+    console.log('File Deleted');
+});
+//Example_2: Checking if the file existed then deleting it using if-else statement
+if(fs.existsSync('./documents/deleteme.txt')){
+    fs.unlink('./documents/deleteme.txt', (err) => {
+        if(err){
+            console.log(err)
+        }
+        console.log(`File Deleted`)
+    });
+}else{
+    fs.writeFile('./documents/deleteme.txt', 'Delete me Please!', () => {
+        console.log('File Created');
+    });
+}
