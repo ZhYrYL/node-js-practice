92802eb982d8b79f543dc769e521f5c0c0ce6776
Zhyryll <zhyryllposadas@gmail.com>
2023-07-22 10:21:41 +0800

    Can now write files and create new files using writeFile() method

--- files.js
+++ files.js
@@ -1,16 +1,30 @@
 const fs = require('fs');
 
-//READ FILES - sa pag read ng files, it takes time kaya kung merong kang kinonsole.log sa files where you read files, uunahin niya yung i-execute
-fs.readFile('./documents/3d.txt', (err, data) => {
-    if(err){
-        console.log(err);
-    }
-    console.log(data.toString());
+//READ FILES - 2 argument. Path and Callback Function with 2 parameters, err and data
+//sa pag read ng files, it takes time kaya kung merong kang kinonsole.log sa files where you read files, uunahin niya yung i-execute
+// fs.readFile('./documents/3d.txt', (err, data) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
+
+// console.log(`Hello World`);
+
+
+
+
+//WRITE FILES - 3 argument yung kailangang ilagay. Path, NewContent, Callback Function. Dapat string yung 2nd argument palagi
+//Example_1: After ma-access yung files, pinalitan yung content
+fs.writeFile('./documents/3d.txt', 'Hello Ella', function () {
+    console.log('Rewritten Already');
 });
 
-console.log(`Hello World`);
+//Example_2: Mag create ng new file, and gumawa ng content
+fs.writeFile('./documents/new.txt', 'Hello World', () => {
+    console.log('Rewritten Already');
+});
 
-//WRITE FILES
 
 
 
