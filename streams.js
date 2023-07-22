73b049d5d9fcb3f1186f41fb2e2b73bf4a3b1751
Zhyryll <zhyryllposadas@gmail.com>
2023-07-22 12:24:17 +0800

    Using pipe() method to transfer content to another file

--- streams.js
+++ streams.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
-
+//STREAM - ginagamit naman yung stream para kahit na hindi pa tapos iread yung isang file, pwede ng gamitin yung ibang chunk of data na na-read
 const readStream = fs.createReadStream('./documents/bigfile.txt', { encoding: 'utf8' });
 const writeStream = fs.createWriteStream('./documents/newbigfile.txt');
 
-readStream.on('data', (chunk) => {
-    console.log('\n-------NEW CHUNK------\n');
-    console.log(chunk);
-    writeStream.write('\n-------NEW CHUNK------\n');
-    writeStream.write(chunk);
-});
+//Nadi-display sa logline yung every new chunk of data kahit di pa siya tapos hanggang sa matapos
+// readStream.on('data', (chunk) => {
+//     console.log('\n-------NEW CHUNK------\n');
+//     console.log(chunk);
+//     writeStream.write('\n-------NEW CHUNK------\n');
+//     writeStream.write(chunk);
+// });
+
+
+//PIPE - simpler version siya ng pagpasa ng file after mag create ng new file. Parang example lang sa taas pero shortcut method. Ang pinagkaiba lang is hindi na siya madi-display sa logline, sa new file agad
+readStream.pipe(writeStream);
+
