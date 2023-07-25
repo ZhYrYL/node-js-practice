bf733fdfe1d31c52f498f5d3ec633ca98eb2cc00
Zhyryll <zhyryllposadas@gmail.com>
2023-07-25 11:26:56 +0800

    Can now send plain and html tags or doc for response

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
-    console.log('Request Made');
+    console.log(req.url, req.method);
+
+    //Response_Example_1: Sending Plain text as Response
+    // res.setHeader('Content-type', 'text/plain');
+    // res.write('Hello Zhyryll Posadas');
+    // res.end();
+
+    //Response_Example_2: Send simple html tags as Response
+    // res.setHeader('Content-type', 'text/html');
+    // res.write('<style>h1{color: red;} h3{color: blue;}</style>');
+    // res.write('<h1>Full Stack Web Developer</h1>');
+    // res.write('<h3>Zhyryll Ryj C. Posadas</h3>');
+    // res.end();
+
+    //Response_Example_3: Sending an HTML document as Response
+    res.setHeader('Content-type', 'text/html');
+    fs.readFile('./views/homepage.html', (err, data) => {
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            res.write(data);
+            res.end();
+        }
+    });
+    
 });
 
 server.listen(3000, 'localhost', () => {
