6279b8f792e06aad918048f2cf3f169bf28c7b95
Zhyryll <zhyryllposadas@gmail.com>
2023-07-28 11:04:34 +0800

    Registering ejs, creating ejs files, and sending response using render() method

--- app.js
+++ app.js
@@ -33,3 +33,17 @@ app.listen(3000);
 
 
 //Response_3: Sending ejs file as response
+app.set('view engine', 'ejs'); //Registering EJS
+
+
+app.get('/', (req, res) => {
+    res.render('homepage');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.use((req, res)=>{
+    res.status(404).render('404');
+});
