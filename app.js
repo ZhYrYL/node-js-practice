8941e60d76053fa8ec2b43ca61ea7dac23ad351a
Zhyryll <zhyryllposadas@gmail.com>
2023-08-05 20:08:55 +0800

    Created another get handler to find all the documents in the database and find document by their ID's

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) =>  app.listen(3000))
     .catch((err)=>{console.log(err)});
 
-
+//Lagyan ng laman yung columns, save, then record sa database
 app.get('/add-blog', (req, res) => {
     const blog = new Blog({
-        title: 'Zhyryll New Blog',
+        title: 'Zhyiee New Blog',
         snippet: 'About my New Blog',
         body: 'More about my new Blog'
     });
@@ -27,6 +27,29 @@ app.get('/add-blog', (req, res) => {
         })  
 });
 
+
+//Itong next get handler will find all documents na recorded sa database
+app.get('/all-blog', (req, res) => {
+    Blog.find()
+        .then((result)=>{
+            res.send(result);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+});
+
+
+//Itong next get handler, hahanapin niya yung document by id
+app.get('/single-blog', (req, res) => {
+    Blog.findById('64ce39f85ce4e35c4cecf90a') //Yung string na nakalagay is id na unique sa kada document na nakasave sa database
+        .then((result)=>{
+            res.send(result);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })  
+});
 
 //Response_1: Sending HTML tags as response using Express
 // app.get('/', (req, res) => {
